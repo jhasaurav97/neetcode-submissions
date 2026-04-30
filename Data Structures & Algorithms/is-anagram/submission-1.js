a0776183d9e9d84f1e56ebd38d644254076d2aab
Saurav Jha <jhasaurav593@gmail.com>
2026-04-30 20:21:21 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length !== t.length) return false;
+        let count = new Array(26).fill(0);
+        for(let char of s){
+            let index = char.charCodeAt(0) - 'a'.charCodeAt(0);
+            count[index]++;
+        }
+        for(let char of t){
+            let index = char.charCodeAt(0) - 'a'.charCodeAt(0);
+            count[index]--;
+        }
+        for(let value of count){
+            if(value !== 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}

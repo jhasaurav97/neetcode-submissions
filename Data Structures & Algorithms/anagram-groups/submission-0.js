f57a9885c3d3348aea0dbca409ecd67c759c786b
Saurav Jha <jhasaurav593@gmail.com>
2026-05-03 23:25:55 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let map = new Map();
+        for(let s of strs){
+            let key = s.split("").sort().join("");
+            if(map.has(key)){
+                map.get(key).push(s);
+            }else{
+                map.set(key, [s]);
+            }
+        }
+    return Array.from(map.values());
+    }
+}
